43bf0b6e4522bfa56d45510b7b02047669791549
sahil shaikh <jazim2211@gmail.com>
2022-08-19 13:56:02 +0530

    added new script

---
+++ helper_scripts/check_var_with_color.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+var1=$1
+var2=$2
+
+if [ ! -z ${var1} ] && [ ! -z $var2 ]
+then
+     echo -e "\e[1;31m *********************************************** \e[0m"
+     echo -e "\e[1;31m PLEASE SELECT ANY ONE VALUE EITHER ALL OR VALUE \e[0m"
+     echo -e "\e[1;31m *********************************************** \e[0m"
+fi
